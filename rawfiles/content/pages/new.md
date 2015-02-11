10723bbde6de05b428495f20c3730bf7e4e4a7e1
Avasz <avashmulmi@gmail.com>
2015-02-11 23:32:18 +0545

    update

---
+++ rawfiles/content/pages/new.md
@@ -0,0 +1,39 @@
+TITLE: About Me
+Date: 2015-02-11 17:28
+Category: About
+Tags: About
+Slug: about
+Authors: Avasz
+Summary: About Me
+
+###Hello World!!  
+
+**Greetings from Nepal!!**
+
+
+
+
+
+
+
+I am Avash Mulmi, usually going by the nick **Avasz** around internet.  
+
+I love __*Open Source*__ technologies and believe in __*Open Web*__.
+
+
+
+
+
+
+
+_Volunteer at [OLE Nepal](http://olenepal.org), [FOSS Nepal](http://fossnepal.org) & [Mozilla](http://mozilla.org)._  
+
+_[Representative of Mozilla](http://reps.mozilla.org/u/avashmulmi)_  
+
+_Studying Computer Engineering in Tribhuvan University_  
+
+_Love to watch cartoons, fictions and such. Yes, I dislike serious ones._
+
+
+
+
---
+++ rawfiles/pelicanconf.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Avasz'
+SITENAME = "/dev/random"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Mozilla', 'http://mozilla.org'),
+        ('Mozilla Nepal', 'http://mozilla-nepal.org'),
+        ('FOSS Nepal', 'http://fossnepal.org'),
+         )
+
+# Social widget
+SOCIAL = (('Facebook', 'http://facebook.com/avasz'),
+        ('Twitter', 'http://twitter.com/avashz'),
+        ('Github', 'http://github.com/avasz'),
+        ('LinkedIN', 'https://np.linkedin.com/pub/avash-mulmi/62/11a/290'),
+        )
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+THEME = 'theme/pelican-bootstrap3'
+BOOTSTRAP_THEME = 'simplex'
+
+USE_FOLDER_AS_CATEGORY = True
+DEFAULT_CATEGORY = 'Others'
+OUTPUT_PATH = '../avasz.github.io/'
+PATH = 'content/'
+PAGE_PATHS=['pages']
+ARTICLE_PATHS=['articles']
+GITHUB_URL = 'http://github.com/avasz/'
+TAG_CLOUD_Steps = 4
+TAG_CLOUD_MAX_ITEMS = 50
+RELATIVE_URLS = True
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+                
